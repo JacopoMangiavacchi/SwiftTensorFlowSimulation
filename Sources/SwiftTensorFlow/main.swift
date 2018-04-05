662437fd5c7f3f462ec998289f0ffb8865c2e919
J-Mangia <j.mangia@ibm.com>
2018-04-05 16:24:08 -0700

    Add operator overloading support

--- Sources/SwiftTensorFlow/main.swift
+++ Sources/SwiftTensorFlow/main.swift
@@ -1,62 +1,90 @@
 import Foundation
 import numsw
 
-public protocol Node {
-    func run() ->  NDArray<Float>?
-}
+class Node {
+    var tf: TensorFlow?
+    var edges: [Node]
 
-open class Constant : Node {
-    internal var result:  NDArray<Float>?
-    internal var edges: [Node]
-
-    public init(_ value:  NDArray<Float>) {
+    init(tf: TensorFlow? = nil) {
+        self.tf = tf
         self.edges = [Node]()
-        self.result = value
     }
 
-    public func run() ->  NDArray<Float>? {
+    func run() ->  NDArray<Float>? {
+        return nil
+    }
+
+    static func +(left: Node, right: Node) -> Node {
+        let x = Add(left, right)
+        left.tf?.nodes.append(x)
+        return x
+    }
+
+    static func -(left: Node, right: Node) -> Node {
+        let x = Subtract(left, right)
+        left.tf?.nodes.append(x)
+        return x
+    }
+
+    static func *(left: Node, right: Node) -> Node {
+        let x = Multiply(left, right)
+        left.tf?.nodes.append(x)
+        return x
+    }
+
+    static func /(left: Node, right: Node) -> Node {
+        let x = Divide(left, right)
+        left.tf?.nodes.append(x)
+        return x
+    }
+}
+
+class Constant : Node {
+    var result:  NDArray<Float>?
+
+    init(_ value:  NDArray<Float>, tf: TensorFlow? = nil) {
+        self.result = value
+        super.init(tf: tf)
+    }
+
+    override func run() ->  NDArray<Float>? {
         return result
     }
 }
 
-open class TwoOperandNode : Node {
-    internal var result:  NDArray<Float>?
-    internal var edges: [Node]
+class TwoOperandNode : Node {
+    var result:  NDArray<Float>?
 
-    init(_ node1: Node, _ node2: Node) {
-        self.edges = [Node]()
+    init(_ node1: Node, _ node2: Node, tf: TensorFlow? = nil) {
+        super.init(tf: tf)
         self.edges.append(node1)
         self.edges.append(node2)
     }
-    
-    public func run() ->  NDArray<Float>? {
-        return result
-    }
 }
 
-open class Add : TwoOperandNode {
-    public override func run() ->  NDArray<Float>? {
+class Add : TwoOperandNode {
+    override func run() ->  NDArray<Float>? {
         guard self.edges.count == 2, let a = self.edges[0].run(), let b = self.edges[1].run() else { return nil }
         return a + b
     }
 }
 
-open class Subtract : TwoOperandNode {
-    public override func run() ->  NDArray<Float>? {
+class Subtract : TwoOperandNode {
+    override func run() ->  NDArray<Float>? {
         guard self.edges.count == 2, let a = self.edges[0].run(), let b = self.edges[1].run() else { return nil }
         return a - b
     }
 }
 
-open class Multiply : TwoOperandNode {
-    public override func run() ->  NDArray<Float>? {
+class Multiply : TwoOperandNode {
+    override func run() ->  NDArray<Float>? {
         guard self.edges.count == 2, let a = self.edges[0].run(), let b = self.edges[1].run() else { return nil }
         return a * b
     }
 }
 
-open class Divide : TwoOperandNode {
-    public override func run() ->  NDArray<Float>? {
+class Divide : TwoOperandNode {
+    override func run() ->  NDArray<Float>? {
         guard self.edges.count == 2, let a = self.edges[0].run(), let b = self.edges[1].run() else { return nil }
         return a / b
     }
@@ -106,9 +134,14 @@ var tf = TensorFlow()
 let a = tf.constant(NDArray<Float>(shape: [3], elements: [5, 7, 10]))
 let b = tf.constant(NDArray<Float>(shape: [3], elements: [2, 3, 21]))
 let c = tf.constant(NDArray<Float>(shape: [3], elements: [3, 5, 7]))
-var d = tf.multiply(a,b)
-var e = tf.add(c,b)
-var f = tf.subtract(d,e)
+
+//var d = tf.multiply(a,b)
+//var e = tf.add(c,b)
+//var f = tf.subtract(d,e)
+
+var d = a * b
+var e = c + b
+var f = d - e
 
 print("result: \(tf.run(f)!)") 
 
